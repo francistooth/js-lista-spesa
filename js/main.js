bd5d7ac307e5519fd5d967a6958c5599337ec3e6
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-11 16:55:21 +0200

    minor: ex complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 // creo lista della spesa
 
+const groceries = ["coke", "frozen pizza", "monster", "cigarettes"];
+
+// prendo gli elementi in pagina e li metto in delle variabili
+
+let divList = document.getElementById("list");
+let ul = document.createElement("ul");
+
 // stampo gli elementi della lista in HTML 
 
-// usando il while
+// creazione variabile per il ciclo while
+
+let i = 0;
+
+// ciclo while
+
+while (i < groceries.length) {
+    let li = document.createElement("li");
+
+    li.appendChild(document.createTextNode(groceries[i]));
+
+    ul.appendChild(li);
+
+    i++;
+}
+
+// usando il while!!
+
+divList.appendChild(ul);
